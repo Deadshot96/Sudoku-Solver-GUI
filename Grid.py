bc7287ab9df32eb8d430d1a2d80157b18b084b38
Bhushan <bhushan1810patil@gmail.com>
2021-02-26 23:39:53 +0530

    added run, quit to Grid

--- Grid.py
+++ Grid.py
@@ -1,6 +1,27 @@
 import pygame
+from Block import Block
+from settings import *
 
 class Grid:
 
     def __init__(self):
+        self.width = WIDTH
+        self.height = HEIGHT
         self.grid = None
+
+    def quit(self):
+        pygame.font.quit()
+        pygame.quit()
+
+    def run(self):
+
+        run = True
+
+        while run:
+            pass
+
+
+
+if __name__ == "__main__":
+    X = Grid()
+    X.run()
