defe8939a3cbb3195e57fb83eb8848c5ddc3bc01
Bhushan <bhushan1810patil@gmail.com>
2021-02-27 18:47:49 +0530

    added select, deselect and is_selected to Block

--- Block.py
+++ Block.py
@@ -7,10 +7,21 @@ class Block:
         self.col = col
         self.size = 50
         self.number = 0
+        self.x = self.size * self.col
+        self.y = self.size * self.row
+        self.selected = False
 
     def set_number(self, number: int) -> None:
         self.number = number
 
+    def select(self) -> None:
+        self.selected = True
 
+    def is_selected(self) -> None:
+        return self.selected
+
+    def deselect(self) -> None:
+        self.selected = False
+    
     def draw(self, win: pygame.Surface):
         pass
