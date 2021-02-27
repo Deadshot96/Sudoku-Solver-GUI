7c84914220e13042b34a4d6d9386d3ffaded625b
Bhushan <bhushan1810patil@gmail.com>
2021-02-27 18:03:28 +0530

    added set_number to Block

--- Block.py
+++ Block.py
@@ -2,10 +2,15 @@ import pygame
 
 class Block:
 
-    def __init__(self, row, col):
+    def __init__(self, row: int, col: int):
         self.row = row
         self.col = col
         self.size = 50
+        self.number = 0
+
+    def set_number(self, number: int) -> None:
+        self.number = number
+
 
     def draw(self, win: pygame.Surface):
         pass
